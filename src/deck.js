91ee0f60ac3bcb8d4f2ca0cd8d0a1d22b58e3fa0
kwmcminn <kwmcminn@gmail.com>
2019-03-26 09:35:02 -0700

    added render function

--- src/deck.js
+++ src/deck.js
@@ -1,21 +1,23 @@
-let suits = ["spades", "diamonds", "clubs", "hearts"];
+let suits = ["♠", "♥", "♣", "♦"];
 let displays = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
 
 class Card {
-   constructor(display, suit, value){
+   constructor(display, suit, value) {
       this.display = display;
       this.suit = suit;
       this.value = value;
    }
 }
 
-function createValue(display){
+function createValue(display) {
    let value = 0;
-   switch (display){
+   switch (display) {
       case "A":
          value = 1;
          break;
-      case "J": case "Q": case "K":
+      case "J":
+      case "Q":
+      case "K":
          value = 10;
          break
       default:
@@ -24,10 +26,10 @@ function createValue(display){
    return value
 }
 
-function createDeck(){
+function createDeck() {
    let newDeck = [];
    suits.forEach(suit => {
-      displays.forEach(display =>{
+      displays.forEach(display => {
          let value = createValue(display)
          let card = new Card(display, suit, value)
          newDeck.push(card)
@@ -36,21 +38,38 @@ function createDeck(){
    return newDeck;
 }
 
-function shuffleDeck(deckArray){
-    let counter = deckArray.length;
+function shuffleDeck(deckArray) {
+   let counter = deckArray.length;
 
-    // While there are elements in the deckArray
-    while (counter > 0) {
-        // Pick a random index
-        let index = Math.floor(Math.random() * counter);
+   // While there are elements in the deckArray
+   while (counter > 0) {
+      // Pick a random index
+      let index = Math.floor(Math.random() * counter);
 
-        // Decrease counter by 1
-        counter--;
+      // Decrease counter by 1
+      counter--;
 
-        // And swap the last element with it
-        let temp = deckArray[counter];
-        deckArray[counter] = deckArray[index];
-        deckArray[index] = temp;
-    }
-    return deckArray;
+      // And swap the last element with it
+      let temp = deckArray[counter];
+      deckArray[counter] = deckArray[index];
+      deckArray[index] = temp;
+   }
+   return deckArray;
+}
+
+function renderCard(cardObject, cardArea) {
+   let newDiv = document.createElement('div');
+   newDiv.textContent = `${cardObject.display}${cardObject.suit}`;
+   newDiv.className = 'card'
+
+   if (["♥", "♦"].includes(cardObject.suit)) {
+      newDiv.classList.add('red')
+   } else {
+      newDiv.classList.add('black')
+   }
+   if (cardArea.id == 'player-cards') {
+      cardArea.appendChild(newDiv);
+   } else {
+      cardArea.appendChild(newDiv);
+   }
 }
--- src/engine.js
+++ src/engine.js
@@ -6,39 +6,30 @@ let dealerCards = document.getElementById('dealer-cards');
 let playerCards = document.getElementById('player-cards');
 
 function newHand() {
-	for (let i = 0; i < 2; i++) {
-		PLAYERHAND[i] = DECK.shift();
-		DEALERHAND[i] = DECK.shift();
-
-		let playerli = document.createElement('li');
-		let dealerli = document.createElement('li');
-
-		playerli.textContent = PLAYERHAND[i].display;
-		if (true) {
-			dealerli.textContent = DEALERHAND[i].display;
-		}
-
-		playerCards.appendChild(playerli);
-		dealerCards.appendChild(dealerli);
-	}
-	console.log(DECK.length);
-	console.log(DEALERHAND);
-	console.log(PLAYERHAND);
+   for (let i = 0; i < 2; i++) {
+      PLAYERHAND[i] = DECK.shift();
+      DEALERHAND[i] = DECK.shift();
+      renderCard(PLAYERHAND[i], playerCards)
+      renderCard(DEALERHAND[i], dealerCards)
+   }
+   console.log(DECK.length);
+   console.log(DEALERHAND);
+   console.log(PLAYERHAND);
 }
 
 function playerHit() {
-	addCard(PLAYERHAND);
-	let playerli = document.createElement('li');
-	playerli.textContent = PLAYERHAND[PLAYERHAND.length - 1].display;
-	playerCards.appendChild(playerli);
-	if (checkForBust(PLAYERHAND)) {
-		console.log('busted');
-		WINNER = 'dealer won';
-		console.log(WINNER + ' won');
-	}
+   addCard(PLAYERHAND);
+   let playerli = document.createElement('li');
+   playerli.textContent = PLAYERHAND[PLAYERHAND.length - 1].display;
+   playerCards.appendChild(playerli);
+   if (checkForBust(PLAYERHAND)) {
+      console.log('busted');
+      WINNER = 'dealer won';
+      console.log(WINNER + ' won');
+   }
 }
 
 function playerStay() {
-	console.log('You Stayed');
-	dealer();
+   console.log('You Stayed');
+   dealer();
 }
