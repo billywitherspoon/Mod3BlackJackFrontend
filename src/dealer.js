1af810764bc7bbbdce94735349cfda761b1f80d2
github username <github email address>
2019-03-26 10:35:02 -0700

    cleaned up dealer and console logs

--- src/dealer.js
+++ src/dealer.js
@@ -3,25 +3,24 @@ function runDealer() {
 		if (handTotal(DEALERHAND) < 8) {
 			addCard(DEALERHAND);
 			renderCard(DEALERHAND[DEALERHAND.length - 1], DEALERCARDSDIV);
-			console.log('dealer hit');
+			console.log('dealer hit on soft hand');
 		} else if (handTotal(DEALERHAND) >= 8) {
 			console.log('dealer stayed on soft 18+');
-			declareWinner();
-			return;
+			break;
 		}
 	}
-	while (handTotal(DEALERHAND) < 17) {
+	while (handTotal(DEALERHAND) < 17 && !isSoft(DEALERHAND)) {
 		// let dealerli = document.createElement('li');
 		// dealerli.textContent = PLAYERHAND[PLAYERHAND.length - 1].display;
 		// DEALERCARDSDIV.appendChild(dealerli);
 		addCard(DEALERHAND);
 		renderCard(DEALERHAND[DEALERHAND.length - 1], DEALERCARDSDIV);
-		console.log('dealer hit');
+		console.log('dealer hit on hard hand');
 		if (isBusted(DEALERHAND)) {
 			console.log('dealer busted');
 			break;
 		}
 	}
-	console.log('dealer stayed on hard hand > 16');
+	console.log('dealer ended it's run);
 	declareWinner();
 }
