a8348e3f7df80a6ba7e48a94b581269f3062b64d
kwmcminn <kwmcminn@gmail.com>
2019-03-28 16:22:03 -0700

    CSS

--- src/bet.js
+++ src/bet.js
@@ -138,6 +138,7 @@ function playerStay() {
 
 function doubleDown() {
 	let doubleDownButton = createHtmlElement('button', '', 'Double Down', 'double-down-button');
+	
 	doubleDownButton.onclick = doubleBet;
 	BETTINGACTIONS.appendChild(doubleDownButton);
 }
